ae07c03d11b635521a4c467135215728202e6397
msestrem <msestrem@gmail.com>
2021-10-08 23:47:25 -0300

    first commit

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -9,5 +9,6 @@ export default function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
+  if(req.method != 'GET') return res.status(400).json({name: "Requisição HTTP inválida"})
   res.status(200).json({ name: req.query.nome })
 }
